44d245c06d06dd31fda373f9591ad7f820a04f2f
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2022-12-22 13:45:14 +0100

    ViewComponent

---
+++ MovieDemo/ViewComponents/StarViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieDemo.Data;
+using MovieDemo.Models.ViewModels;
+
+namespace MovieDemo.ViewComponents
+{
+    public class StarViewComponent : ViewComponent
+    {
+        //private readonly MovieDemoContext movieDemoContext;
+
+        //public StarViewComponent(MovieDemoContext movieDemoContext)
+        //{
+        //    this.movieDemoContext = movieDemoContext;
+        //}
+
+
+        public IViewComponentResult Invoke(float rating)
+        {
+            var doubleRating = (int)Math.Round(rating * 2);
+
+            var model = new StarViewModel
+            {
+                Stars = doubleRating / 2,
+                IsHalfStar = doubleRating % 2 == 1
+            };
+
+            return View(model);
+        }
+    }
+}
